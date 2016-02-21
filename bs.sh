9326f01f0110eba8f5b69da3336f76f9bb852a5c
Michal Blazek <mic.blazek@gmail.com>
2016-02-21 13:47:44 +0100

    * Add help * Add uninstall application (must run script twice) * Edit script output

--- bs.sh
+++ bs.sh
@@ -1,29 +1,62 @@
 # !/bin/bash
 # file: bs.sh, bs_instaler.sh, bs_lite.apk
 # Author:	Michal Blažek
-# Date:		10.12.2015
+# Date:		21.2.2015
 
 # Před použitím
 # Povolit instalaci z cizích zdrojů
 
-#Instalace bs_lite do systémových aplikací
-adb push bs_lite.apk /mnt/sdcard/
-echo bs_lite.apk --- Nahran do /mnt/sdcard/bs_lite.apk
+#Parametry:
+		# $1 parametr - 1 = nainstalovat systémovou aplikaci
+		# $1 parametr - 0 = odinstalovat systémovou aplikaci
 
-#Instalace "velkého" Blindshellu (první parametr)
-echo Instaluji $2 - může to chvilku trvat
-adb install $2
-echo INSTALACE DOKONČENA--- $2
+		# $2 parametr - cesta k apk aplikace
 
-#Nahrání nastavovacího scriptu 
-adb push bs_instaler.sh /mnt/sdcard/
-echo bs_instaler.sh --- Nahran do /mnt/sdcard/bs_instaler.sh
 
-#Spuštění nastavovacího scriptu 
-adb shell su sh /mnt/sdcard/bs_instaler.sh $1
-echo bs_instaler.sh --- Hotovo
+if [ -n "$1" ]
+	then
+	if [ $1 = -h ]
+		then
+		echo "\n[First parametr]: \ndetermines instalation (set 1) or unstalation (set 0) priv-system app bs_lite.apk."
+		echo "\n[Second parametr]: \ndetermines path to full Blindshell.apk. This .apk have to signed by same signiture as bs_lite.apk.\n"
 
-#adb shell su reboot
-#adb shell su rm /mnt/sdcard/bs_instaler.sh
+	else
+		#Instalace bs_lite do systémových aplikací
+		adb push bs_lite.apk /mnt/sdcard/ 
+		echo bs_lite.apk --- Nahran do /mnt/sdcard/bs_lite.apk 
 
-#echo bs_instaler.sh --- Uklizeno 
+		#Instalace "velkého" Blindshellu (první parametr)
+		if [ -n "$2" ]
+			then
+				echo Instaluji $2 - může to chvilku trvat
+				adb install $2 
+				echo INSTALACE DOKONČENA--- $2
+		fi
+
+		if [ $1 = 0 ]
+			then
+				echo Odintaluji 
+				adb shell am start -a android.intent.action.DELETE -d "package:com.matapo.blindshell"
+				adb uninstall com.matapo.blindshell
+				echo ODINSTALOVÁNO
+		fi
+
+		#Nahrání nastavovacího scriptu 
+		adb push bs_instaler.sh /mnt/sdcard/ 
+		echo bs_instaler.sh --- Nahran do /mnt/sdcard/bs_instaler.sh "\n\t"
+
+		#Spuštění nastavovacího scriptu 
+		adb shell su sh /mnt/sdcard/bs_instaler.sh $1 
+		echo bs_instaler.sh --- Hotovo
+
+		#adb shell su reboot
+		adb shell su rm -f -i -r -d /mnt/sdcard/bs_instaler.sh > text.txt
+		adb shell su rm -f -i -r -d /mnt/sdcard/bs_lite.apk > text.txt
+
+		echo bs_instaler.sh --- Uklizeno
+		echo bs_lite.apk --- Uklizeno 
+	fi
+else
+	echo "\n[First parametr]: \ndetermines instalation (set 1) or unstalation (set 0) priv-system app bs_lite.apk."
+	echo "\n[Second parametr]: \ndetermines path to full Blindshell.apk. This .apk have to signed by same signiture as bs_lite.apk.\n"
+fi
--- bs_instaler.sh
+++ bs_instaler.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 # file: bs.sh, bs_instaler.sh, bs_lite.apk
 # Author:	Michal Blažek
-# Date:		10.12.2015
+# Date:		21.2.2016
 
 echo START bs_instaler 
 wifi=0				#1 - set WiFI enable , 0 - set WiFI diable
@@ -15,103 +15,144 @@ brightness=130		#Jas - 1300 je 50%
 
 instal_remove_BS_lite=$1
 
-instalBs_lite=0		#1=nainstalovat bs_lite.apk, 0 = neinstalovat
-removeBs_lite=1 	#1=odinstalovat bs_lite.apk, 0 = neodinstalovat
+showSystemResponse=1
 
 #Instalace bs_lite.apk
-if [ $instal_remove_BS_lite = 1 ]
+if [ $1 == 1 ]
 then
-mount -o remount,rw /system
-cp /storage/sdcard0/bs_lite.apk /system/priv-app/
-cd /system/priv-app/
-chmod 644 bs_lite.apk
-mount -o remount, ro /system
-echo ----------------
-echo bs_lite.apk - Nainstalovano
-ls -l bs_lite.apk
-echo ----------------
-cd /
+	mount -o remount,rw /system
+	cp /storage/sdcard0/bs_lite.apk /system/priv-app/
+	cd /system/priv-app/
+	chmod 644 bs_lite.apk
+	mount -o remount, ro /system
+	echo "\t"----------------
+	echo "\t"bs_lite.apk - Nainstalovano
+	ls -l bs_lite.apk
+	echo "\t"----------------"\n"
+	cd /
 fi
 
 #Odinstalace bs_lite.apk
 if [ $instal_remove_BS_lite = 0 ]
 then
-mount -o remount,rw /system
-rm /system/priv-app/bs_lite.apk
-mount -o remount, ro /system
-echo ----------------
-echo bs_lite.apk - Odinstalováno
-echo ----------------
+	mount -o remount,rw /system
+	rm /system/priv-app/bs_lite.apk
+	mount -o remount, ro /system
+	echo "\t"----------------
+	echo "\t"bs_lite.apk - Odinstalováno
+	echo "\t"----------------"\n"
 fi
 
 #WIFI
 if [ $wifi = 0 ]
 	then
-	svc wifi disable
-	echo ----------------
-	echo WiFi --- Disable
-	echo ----------------
+	echo "\t"----------------
+	echo "\t"WiFi --- Disable
+	if [ $showSystemResponse = 0 ]
+		then
+		svc wifi disable > /dev/null
+	else
+		svc wifi disable
+	fi
+	echo "\t"----------------"\n"
 else
-	svc wifi enable
-	echo ----------------
-	echo WiFi --- Enable
-	echo ----------------
+	echo "\t"----------------
+	echo "\t"WiFi --- Enable
+	if [ $showSystemResponse = 0 ]
+		then
+		svc wifi enable > /dev/null
+	else
+		svc wifi enable
+	fi
+	echo "\t"----------------"\n"
 fi
 
 #Mobile Data
 if [ $mobileData = 0 ]
-	then
-	svc data disable
-	echo ----------------
-	echo Mobile Data --- Disable
-	echo ----------------
+	then	
+	echo "\t"----------------
+	echo "\t"Mobile Data --- Disable
+	if [ $showSystemResponse = 0 ]
+		then
+		svc data disable > /dev/null
+	else
+		svc data disable
+	fi
+	echo "\t"----------------"\n"
 else
-	svc data enable
-	echo ----------------
-	echo Mobile Data --- Enable
-	echo ----------------
+	echo "\t"----------------
+	echo "\t"Mobile Data --- Enable
+	if [ $showSystemResponse = 0 ]
+		then
+		svc data enable > /dev/null
+	else
+		svc data enable
+	fi
+	echo "\t"----------------"\n"
 fi
 
 #Bluetooth
 if [ $bluetooth = 0 ]
-	then
-	su -c service call bluetooth_manager 8
-	echo ----------------
-	echo Bluetooth --- Disable
-	echo ----------------
+	then	
+	echo "\t"----------------
+	echo "\t"Bluetooth --- Disable
+	if [ $showSystemResponse = 0 ]
+		then
+		su -c service call bluetooth_manager 8 > /dev/null
+	else
+		su -c service call bluetooth_manager 8 
+	fi
+	echo "\t"----------------"\n"
 else
-	su -c service call bluetooth_manager 6
-	echo ----------------
-	echo Bluetooth --- Enable
-	echo ----------------
+	echo "\t"----------------
+	echo "\t"Bluetooth --- Enable
+	if [ $showSystemResponse = 0 ]
+		then
+		su -c service call bluetooth_manager 6 > /dev/null
+	else
+		su -c service call bluetooth_manager 6 
+	fi
+	echo "\t"----------------"\n"
 fi
 
 #NFC
 if [ $nfc = 0 ]
 	then
-	service call nfc 7
-	echo ----------------
-	echo NFC --- Disable
-	echo ----------------
+	#service call nfc 7 
+	echo "\t"----------------
+	echo "\t"NFC --- Disable
+	if [ $showSystemResponse = 0 ]
+		then
+		su -c service call nfc 7 > /dev/null
+	else
+		su -c service call nfc 7 
+	fi
+	echo "\t"----------------"\n"
 else
-	service call nfc 8
-	echo ----------------
-	echo NFC --- Enable
-	echo ----------------
+	#service call nfc 8 
+	echo "\t"----------------
+	echo "\t"NFC --- Enable
+	if [ $showSystemResponse = 0 ]
+		then
+		su -c service call nfc 8  > /dev/null
+	else
+		su -c service call nfc 8  
+	fi
+	echo "\t"----------------"\n"
 fi
 
 #GPS
 if [ $gps = 0 ]
 	then
-	settings put secure location_providers_allowed ' '
-	echo ----------------
-	echo GPS --- Disable
-	echo ----------------
+	echo "\t"----------------
+	echo "\t"GPS --- Disable
+	settings put secure location_providers_allowed ' ' 
+	echo "\t"----------------"\n"
 else
-	settings put secure location_providers_allowed gps,network
-	echo ----------------
-	echo GPS --- Enable
-	echo ----------------
+	echo "\t"----------------
+	echo "\t"GPS --- Enable
+	settings put secure location_providers_allowed gps,network 
+	echo "\t"----------------"\n"
 fi
 
 #Vypnout zvuky tlacitek
@@ -124,21 +165,36 @@ fi
 pm disable com.google.android.systemupdater
 
 #Nastavit limit dipleje na 5min
-sqlite3 /data/data/com.android.providers.settings/databases/settings.db "update system set value='300000' where name='screen_off_timeout';"
+if [ $showSystemResponse = 0 ]
+		then
+		sqlite3 /data/data/com.android.providers.settings/databases/settings.db "update system set value='300000' where name='screen_off_timeout';"  > /dev/null
+	else
+		sqlite3 /data/data/com.android.providers.settings/databases/settings.db "update system set value='300000' where name='screen_off_timeout';"  
+fi
 
 #Nastavit jas na 50%
-settings put system screen_brightness $brightness
-echo ----------------
-echo Brightness is $brightness
-echo ----------------
+echo "\t"----------------
+echo "\t"Brightness is $brightness
+if [ $showSystemResponse = 0 ]
+		then
+		settings put system screen_brightness $brightness  > /dev/null
+	else
+		settings put system screen_brightness $brightness  
+	fi
+echo "\t"----------------"\n"
 
 #Zruseni zamku displeje - set na NONE
 if [ $noneLockScreen = 0 ]
 	then
-	sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '1' WHERE name = 'lockscreen.disabled';"
-	echo ----------------
-	echo Screen  --- NONE
-	echo ----------------
+	if [ $showSystemResponse = 0 ]
+		then
+		sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '1' WHERE name = 'lockscreen.disabled';"   > /dev/null
+	else
+		sqlite3 /data/system/locksettings.db "UPDATE locksettings SET value = '1' WHERE name = 'lockscreen.disabled';"   
+	fi
+	echo "\t"----------------
+	echo "\t"Screen  --- NONE
+	echo "\t"----------------"\n"
 fi
 
 #OTHER
